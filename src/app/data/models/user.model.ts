acc1f9c0574765c9d33beb5ec1cd1cb08fb5633a
Alex dos Santos Moura <asmoura@sefaz.se.gov.br>
2020-09-10 14:38:54 -0300

    Implementação dos modelos: Address, Geolocation e Company

--- src/app/data/models/user.model.ts
+++ src/app/data/models/user.model.ts
@@ -1,10 +1,13 @@
+import { Address } from './address.model';
+import { Company } from './company.model';
+
 export interface User {
     id: number;
     name: string;
     username: string;
     email: string;
+    address: Address;
     phone: string;
     website: string;
-    // address: Address;
-    // company: Company;
+    company: Company;
 }
